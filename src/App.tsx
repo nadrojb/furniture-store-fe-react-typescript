cae3103143515c829bdb2bdbcdc8ecd15b5adb0f
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-03 20:28:20 +0000

    fetched categories from api, can now console log all categories

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [categories, setCategories] = useState([]);
 
-  return <>
-  </>;
+
+
+  useEffect(() => {
+    fetch("http://localhost:3001/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
+
+  function renderCategories () {
+
+  }
+
+  return <></>;
 }
 
 export default App;
