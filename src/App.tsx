b97f074c6cb7b87092b8490ecced669f9c9e8eb7
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-04 10:11:58 +0000

    added interface for categories data

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+interface categoriesData {
+  id: number;
+  name: string;
+  products: number;
+  image: string;
+}
+
 function App() {
-  const [categories, setCategories] = useState([]);
-
-
+  const [categories, setCategories] = useState<categoriesData>({
+    id: 1,
+    name: "chairs",
+    products: 10,
+    image: "https://placehold.co/600x400/png",
+  });
 
   useEffect(() => {
     fetch("http://localhost:3001/categories")
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
+
         setCategories(data);
       });
   }, []);
 
-  function renderCategories () {
-
-  }
+  function renderCategories() {}
 
   return <></>;
 }
