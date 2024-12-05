2055e16843d6da0f80526f94b72455abfda036ff
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-05 15:47:55 +0000

    created header and added styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,16 @@ import { NotFound } from "./Components/NotFound";
 import ProductsPage from "./Components/ProductsPage";
 import UserContext from "./Contexts/UserContext";
 import SingleProductPage from "./Components/SingleProductPage";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
+
+
+
 
 function App() {
   const [currentUser, setCurrentUser] = useState("biff");
@@ -16,8 +26,20 @@ function App() {
           username: "biff",
         }}
       >
+        <header className="flex justify-between mx-3 align-baseline my-3">
+          <div>
+            <h1 className="font-mono text-3xl font-semibold text-gray-800">Fuhrn</h1>
+          </div>
+          <div className="text-2xl text-gray-800">
+          <FontAwesomeIcon icon={faCartShopping}/>
+          <FontAwesomeIcon icon={faBars} className="ml-3"/>
+          </div>
+        </header>
+          <form action="" className="w-full mx-auto text-center px-3 mb-4">
+            <input type="text" placeholder="Search" className="w-2/3 bg-gray-200 py-2 px-2 rounded-l-lg" />
+            <input type="submit" className="w-1/3 bg-gray-800 text-white py-2 px-2 rounded-r-lg"/>
+          </form>
         <BrowserRouter>
-          <Link to="/">Home</Link>
           <Routes>
             <Route path="products/:catId" element={<ProductsPage />} />
             <Route path="product/:productId" element={<SingleProductPage />} />
