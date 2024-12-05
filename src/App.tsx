d9471b058c423365782c6bfda9c4cce1573797d7
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-05 10:16:20 +0000

    added a context example

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { HomePage } from "./Components/HomePage";
 import { NotFound } from "./Components/NotFound";
 import { ProductsPage } from "./Components/ProductsPage";
+import UserContext from "./Contexts/UserContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("biff");
   return (
     <>
-      <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Routes>
-          <Route path="products/:catId" element={<ProductsPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <UserContext.Provider
+        value={{
+          username: "biff",
+        }}
+      >
+        <BrowserRouter>
+          <Link to="/">Home</Link>
+          <Routes>
+            <Route path="products/:catId" element={<ProductsPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </>
   );
 }
--- src/Components/CategoriesCard/index.tsx
+++ src/Components/CategoriesCard/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { categoriesData } from "../HomePage";
-
+import { useContext } from "react";
+import UserContext from "../../Contexts/UserContext";
 
 export default function CategoriesCard({
   id,
@@ -8,8 +9,12 @@ export default function CategoriesCard({
   products,
   image,
 }: categoriesData) {
+  const { username } = useContext(UserContext);
   return (
     <>
+      <div>
+        <p>Welcome back {username}</p>
+      </div>
       <div>
         <img src={image} alt={name} />
         <h3>{name}</h3>
