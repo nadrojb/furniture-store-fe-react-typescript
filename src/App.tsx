a8e4d9905d303cc91b4e895cf19e32d71e76447a
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-05 18:29:07 +0000

    altered the css for header

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ import ProductsPage from "./Components/ProductsPage";
 import UserContext from "./Contexts/UserContext";
 import SingleProductPage from "./Components/SingleProductPage";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-
 
 function App() {
   const [currentUser, setCurrentUser] = useState("biff");
@@ -20,19 +19,33 @@ function App() {
           username: "biff",
         }}
       >
-        <header className="flex justify-between mx-3 align-baseline my-3">
-          <div>
-            <h1 className="font-mono text-3xl font-semibold text-gray-800">Fuhrn</h1>
+        <header className="align-baseline my-3">
+          <div className="flex justify-between mb-3 px-3">
+            <h1 className="font-mono text-3xl font-semibold text-gray-800">
+              Fuhrn
+            </h1>
+
+            <div className="text-2xl text-gray-800">
+              <FontAwesomeIcon icon={faCartShopping} />
+              <FontAwesomeIcon icon={faBars} className="ml-3" />
+            </div>
           </div>
-          <div className="text-2xl text-gray-800">
-          <FontAwesomeIcon icon={faCartShopping}/>
-          <FontAwesomeIcon icon={faBars} className="ml-3"/>
-          </div>
-        </header>
-          <form action="" className="w-full mx-auto text-center px-3 pb-4 shadow-xs border-b-2 mb-4">
-            <input type="text" placeholder="Search" className="w-2/3 bg-gray-200 py-2 px-2 rounded-l-lg" />
-            <input type="submit" className="w-1/3 bg-gray-800 text-white py-2 px-2 rounded-r-lg"/>
+
+          <form
+            action=""
+            className="w-full mx-auto text-center pb-4 shadow-xs border-b-2 mb-4 px-3"
+          >
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-2/3 bg-gray-200 py-2 px-2 rounded-l-lg"
+            />
+            <input
+              type="submit"
+              className="w-1/3 bg-gray-800 text-white py-2 px-2 rounded-r-lg"
+            />
           </form>
+        </header>
         <BrowserRouter>
           <Routes>
             <Route path="products/:catId" element={<ProductsPage />} />
